675cb1e6ac7edf0868ec29b1909a4ef4893f04e6
Fu Zuoyou <fupolarbear@gmail.com>
2014-01-14 23:37:19 +0800

    meow

--- popup.js
+++ popup.js
@@ -3,39 +3,17 @@ $(document).ready(function() {
 });
 
 function getHistory() {
-	chrome.history.search({
-    	'text': '',
-        'maxResults': 10,
-        'startTime': 0
-    }, historyfunc);
-}
-
-function historyfunc(results){
-	$("body").append('<p>hehe</p>');
-	for(i in results){
-		var hisStr = "<p>"
-			+ results[i]['id']
-			+ results[i]['lastVisitTime']
-		 	+ transTimeToDate(results[i]['lastVisitTime'])
-		 	+ results[i]['title']
-		 	+ "</p>";
-		$("body").append(hisStr);
-	}
-}
-
-function transTimeToDate(time){
-	var timeStr = "";
-	var date = new Date(time);
-	timeStr += date.getFullYear();
-	timeStr += "-";
-	timeStr += date.getMonth();
-	timeStr += "-";
-	timeStr += date.getDate();
-	timeStr += "-";
-	timeStr += date.getHours();
-	timeStr += "-";
-	timeStr += date.getMinutes();
-	timeStr += "-";
-	timeStr += date.getSeconds();
-	return timeStr;
+	chrome.storage.local.get('table', function(item){
+		if(item.table){
+			var p = '';
+			var q = 1;
+			for(k in item.table){
+				p = p + ((q%2==0)?'<tr><td>':'<tr class="odd"><td>') + k + '</td><td>' + (item.table[k]/60).toFixed(3) + '</td></tr>';
+				q++;
+			}
+			$("body").append('<table border="1" align="center" id="hor-zebra"><caption>Time I wasted on this sites (by pb. just play around:D)</small></caption><tr><th>site</th><th>time(min)</th></tr>' + p + '</table>');
+		} else {
+			$("body").append("<p>No data now =w=</p>");
+		}
+	});
 }
--- timer.js
+++ timer.js
@@ -1,6 +1,20 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-	console.log('Turning ' + tab.url + ' red!');
-	chrome.tabs.executeScript({
-		code: 'document.body.style.backgroundColor="red"'
-	});
-});
+var rand = 5000;
+
+(function loop() {
+	//console.log("sleep: " + rand);
+	var hehe = function() {
+			chrome.runtime.sendMessage(
+				{ greeting: "hello", url: document.domain },
+				function(response) {
+					//console.log('meow', response);
+					if(response.farewell){
+						rand = 5000;
+					} else {
+						rand = 5000;
+					}
+				}
+			);
+			loop();
+	}
+	did = setTimeout(hehe, rand);
+}());
